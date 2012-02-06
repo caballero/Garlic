84ca5d01318f1c5e6051a692bc3149191163c8d1
Juan Caballero <jcaballero@systemsbiology.org>
2012-02-06 12:35:04 -0800

    Simple repeats freq hack

--- bin/createFakeSequenceBin.pl
+++ bin/createFakeSequenceBin.pl
@@ -831,7 +831,7 @@ sub insertElements {
 		$new = $ins[ int(rand @ins) ];
 	    print "selected: $new\n" if (defined $debug);
 	    if ($new =~ m/SIMPLE/) {
-			next if ( 0.99 > rand); # hack to avoid SIMPLE repeats high density
+			next if ( 0.999 > rand); # hack to avoid SIMPLE repeats high density
             $seq = evolveSimple($new, $gc);
             $usim++;
         }
