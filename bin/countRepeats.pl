ea44ac2e58661caa12cc02a33e6645e8e3f95c0e
Juan Caballero <juan.caballero.perez@gmail.com>
2014-03-11 13:21:52 -0600

    repetitive elements stat scripts

---
+++ bin/countRepeats.pl
@@ -0,0 +1,46 @@
+#!/usr/bin/perl 
+
+use strict;
+use warnings;
+
+my %rep;
+my %fam;
+my $nseq = -1;
+while (<>) {
+	if (m/^#/) {
+		$nseq++;
+		next;
+    }
+	chomp;
+	my ($pos, $rep) = split (/\t/, $_);
+	my @rep = split (/,/, $rep);
+	foreach my $r (@rep) {
+		if ($r =~ m/SIMPLE/) {
+			$rep{$nseq}{'SIMPLE'}++;
+			$fam{'SIMPLE'} = 1;
+			
+		}
+		else {
+			my ($type, $fam) = split (/:/, $r);
+			$fam =~ s/\/.*//;
+			$rep{$nseq}{$fam}++;
+			$fam{$fam} = 1;
+		}
+	}
+}
+
+my @fam = sort keys %fam;
+print join "\t", 'Seq', @fam;
+print "\n";
+
+for (my $i = 0; $i <= $nseq; $i++) {
+	my $n = $i;
+	$n = "0$n" if ($i < 10);
+	print "fake$n";
+	foreach my $fam (@fam) { 
+		my $cnt = 0;
+		$cnt = $rep{$i}{$fam} if (defined $rep{$i}{$fam});
+		print "\t$cnt";
+	}
+	print "\n";
+}
